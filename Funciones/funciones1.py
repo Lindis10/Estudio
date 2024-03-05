16e3b2a5fe3250a7bd499a6ab8d5e06b11b433a5
Fabio Anaya <_Fabio.Anaya.ext.AZ@sodexo.com>
2024-03-05 11:46:20 -0500

    Carpeta funciones

---
+++ Funciones/funciones1.py
@@ -0,0 +1,9 @@
+def saludar(nombre, adjetivos):
+    print(f"Hola {nombre}, cómo estás? Aparte de {adjetivos[0]} y {adjetivos[1]}")
+
+def elevar_al_cuadrado(numero):
+    return numero * numero
+
+# saludar("Linda", ["Bonita", "Hermosa"])
+valor = elevar_al_cuadrado(2)
+print(valor)
